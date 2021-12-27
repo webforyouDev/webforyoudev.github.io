1da785ccd093eb19b8af41643b0bcba002bb8bd8
Furkan Ergin <furkann98@hotmail.com>
2021-12-27 14:50:08 +0100

    Npm createReactApp - Kommer snart!

---
+++ webforyou/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Web For You</h3>
+        <p>Skreddersydde IT-løsninger</p>
+        <p>Kommer snart!</p>
+        <p>Web.For.You@hotmail.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
